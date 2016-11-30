84c722f4353ead86f77b96bb80bbe07383eec96f
Marco <jesusmarcodelcarmen@gmail.com>
2016-11-30 11:37:12 -0600

    Fixed apis

---
+++ routes/volunteer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.displayVolunteerPage = (req, res) => {
+  //display something
+}
+
+exports.displayVolunteerHospital = (req, res) => {
+  //display something
+}
+
+exports.displayVolunteerFoodbank = (req, res) => {
+  //display something
+}
+
+exports.displayVolunteerAnimalShelter = (req, res) => {
+  //display something
+}
