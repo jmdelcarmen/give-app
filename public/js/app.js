4ba959d10ead27b27e2464487c384671b0c1c59c
Marco <jesusmarcodelcarmen@gmail.com>
2016-11-30 13:54:50 -0600

    setup angular

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+      angular.module('giveItApp', [])
+
+      //main controller
+      .controller('mainCtrl', ['$scope', '$http', function ($scope, $http) {
+        //angular functions and scope stuff go here
+
+
+      }]);
+}());
