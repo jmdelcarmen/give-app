f906fe22ca77f9f1be7791fdc8fe2f073fd82531
bururik <spacepopeleo@gmail.com>
2016-11-30 13:55:18 -0600

    created a schema, added files to the git ignore, and fixed a bit of syntax throughout code for readability

--- models/donation.js
+++ models/donation.js
@@ -3,6 +3,44 @@
 //donation model here
 const mongoose = require('mongoose');
 
+let donSchm = mongoose.Schema({
+    name : String,
 
+    category : String,
 
-module.exports = mongoose.model(<MODELNAME>, <SCHEMA>);
+    location : {
+      lat : Number,
+      lng : Number
+    },
+
+    website_url : String,
+
+    image : String,
+
+    description : String,
+
+    contactUs : [{
+        name : String,
+        title : String,
+        phoneNumber : String
+    }],
+
+    address : {
+        street : String,
+        city : String,
+        state : String,
+        zip : String
+    },
+
+    hours : [{
+        days : String,
+        hours : String,
+        day : String,
+        hour : String
+    }],
+
+    open_closed: Boolean,
+    
+});
+
+module.exports = mongoose.model( 'Donation' , donSchm );
