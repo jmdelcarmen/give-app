2d15748990f4c15710129536b9460a3dfd7d974f
Marco <jesusmarcodelcarmen@gmail.com>
2016-12-02 10:44:12 -0600

    Added regex for each query

--- routes/donation.js
+++ routes/donation.js
@@ -12,43 +12,13 @@ exports.displayDonationPage = (req, res) => {
   });
 }
 
-exports.displayDonationFoodbank = (req, res) => {
-  // var val = new RegExp('foodbank');
-  Donation.find({name: {$regex: /*val*//foodbank/, $options: 'ig'}}, (err, donations) => {
+exports.displayDonationCategory = (req, res) => {
+  let category = new RegExp(req.params.category.replace('_', ' '));
+  Donation.find({name: {$regex: category, $options: 'wsig'}}, (err, donations) => {
     if (err) {
       res.status(500).send('Failed to load donations.');
     } else {
-      res.render('donations/foodbank', {donations: donations, key: process.env.MAP_KEY});
-    }
-  });
-}
-
-exports.displayDonationClothesAndToys = (req, res) => {
-  Donation.find({name: {$regex: /goodwill/, $options: 'ig'}}, (err, donations) => {
-    if (err) {
-      res.status(500).send('Failed to load donations.');
-    } else {
-      res.render('donations/clothesandtoys', {donations: donations});
-    }
-  });
-}
-
-exports.displayDonationBlood = (req, res) => {
-  Donation.find({name: {$regex: /blood/, $options: 'ig'}}, (err, donations) => {
-    if (err) {
-      res.status(500).send('Failed to load donations.');
-    } else {
-      res.render('donations/blood', {donations: donations});
-    }
-  });
-}
-
-exports.displayDonationPlasma = (req, res) => {
-  Donation.find({name: {$regex: /blood services/, $options: 'sig'}}, (err, donations) => {
-    if (err) {
-      res.status(500).send('Failed to load donations.');
-    } else {
-      res.render('donations/plasma', {donations: donations});
+      res.render('donation/donation-category', {donations: donations, key: process.env.MAP_KEY});
     }
   });
 }
