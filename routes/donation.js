c79c16aa8eef381c3b920da248d79e0c8a2605b9
bururik <spacepopeleo@gmail.com>
2016-12-03 19:01:32 -0600

    making adjustments to maps, front end, routes, and inputing images. Some of this commit is a step back, but only to adjust an issue with the maps; this means some of this commit will be rewritten in the next commit

--- routes/donation.js
+++ routes/donation.js
@@ -12,13 +12,37 @@ exports.displayDonationPage = (req, res) => {
   });
 }
 
-exports.displayDonationCategory = (req, res) => {
-  let category = new RegExp(req.params.category.replace('_', ' '));
-  Donation.find({name: {$regex: category, $options: 'wsig'}}, (err, donations) => {
+// exports.displayDonationCategory = (req, res) => {
+//   let category = new RegExp(req.params.category.replace('_', ' '));
+//   Donation.find({name: {$regex: category, $options: 'wsig'}}, (err, donations) => {
+exports.displayDonationFoodbank = (req, res) => {
+  // var val = new RegExp('foodbank');
+  Donation.find({name: {$regex: /*val*//foodbank/, $options: 'ig'}}, (err, donations) => {
     if (err) {
       res.status(500).send('Failed to load donations.');
     } else {
-      res.render('donation/donation-category', {donations: donations, key: process.env.MAP_KEY});
+      // res.render('donation/donation-category', {donations: donations, key: process.env.MAP_KEY});
+      res.render('donation/foodbank', {donations: donations, key: process.env.MAP_KEY});
+    }
+  });
+}
+
+exports.displayDonationClothesAndToys = (req, res) => {
+  Donation.find({name: {$regex: /goodwill/, $options: 'ig'}}, (err, donations) => {
+    if (err) {
+      res.status(500).send('Failed to load donations.');
+    } else {
+      res.render('donation/clothesandtoys', {donations: donations});
+    }
+  });
+}
+
+exports.displayDonationBlood = (req, res) => {
+   Donation.find({name: {$regex: /blood/, $options: 'ig'}}, (err, donations) => {
+     if (err) {
+       res.status(500).send('Failed to load donations.');
+     } else {
+       res.render('donation/blood', {donations: donations});
     }
   });
 }
